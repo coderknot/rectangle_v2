82189f8749d80d5f30a2f4adcb955fc54618fadf
CoderKnot <coder.knot@gmail.com>
2017-03-16 23:04:40 -0700

    add test for returning Rectangle length

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,6 +1,12 @@
 public class Rectangle {
 
-  public Rectangle(int length, int width) {
+  private int mLength;
 
+  public Rectangle(int length, int width) {
+    mLength = length;
+  }
+
+  public int getLength() {
+    return mLength;
   }
 }
